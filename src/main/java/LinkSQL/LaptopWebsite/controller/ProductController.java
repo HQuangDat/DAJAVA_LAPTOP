ba2434ce537eecc42e34cdb32fd140aa10aad80f
RidGud <hoquangdat123@gmail.com>
2023-06-20 09:08:56 +0700

    delete unused library

--- src/main/java/LinkSQL/LaptopWebsite/controller/ProductController.java
+++ src/main/java/LinkSQL/LaptopWebsite/controller/ProductController.java
@@ -1,18 +1,15 @@
 package LinkSQL.LaptopWebsite.controller;
 
-import LinkSQL.LaptopWebsite.model.Category;
 import LinkSQL.LaptopWebsite.model.Product;
 import LinkSQL.LaptopWebsite.services.ProductService;
 import LinkSQL.LaptopWebsite.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.Optional;
 
 @Controller
--- src/main/java/LinkSQL/LaptopWebsite/model/Category.java
+++ src/main/java/LinkSQL/LaptopWebsite/model/Category.java
@@ -3,7 +3,6 @@ package LinkSQL.LaptopWebsite.model;
 import jakarta.persistence.*;
 import lombok.Data;
 
-import java.util.List;
 
 @Data
 @Entity
